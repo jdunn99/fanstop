3d056a724b6b8f2bff1bb25ca3ae1f8081f41856
Jack Dunn <jmhjdunn123@outlook.com>
2024-02-06 13:21:39 -0500

    removed one more unused file

--- lib/queries/useProfile.tsx
+++
@@ -1,8 +0,0 @@
-import { Profile } from "@/pages/api/communities/[communityId]/profile";
-import { useQuery } from "react-query";
-
-export function useProfileQuery(id: string) {
-  return useQuery<Profile>(["profile"], () =>
-    fetch(`/api/communities/${id}/profile`).then((res) => res.json())
-  );
-}
